07a2ab514e12380fd690ef3310eff71efd7952a0
Joseorina <orinangare@gmail.com>
2019-10-07 04:59:44 +0300

    [Feature cursor]

--- db_config.py
+++ db_config.py
@@ -6,17 +6,30 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 class PostgresConfig(Postgres):
 
-    def connect(self, database_url):
-        """
-        Create a connection to postgres database        
-        Arguments:
-            database_url {URl containing user login details}
-        """
+	def connect(self, database_url):
+		"""
+		Create a connection to postgres database        
+		Arguments:
+			database_url {URl containing user login details}
+		"""
 
-        try:
-            connection = p.connect(database_url)
-            connection.autocommit = True
-            return connection
+		try:
+			connection = p.connect(database_url)
+			connection.autocommit = True
+			return connection
 
-        except:
-            return 'failed to connect to database' 
+		except:
+			return 'failed to connect to database'
+
+	def cursor(self):
+		"""
+		Create cursor object allowing execution og Postgresql commands
+		"""
+		try:
+			query = f"""CREATE DATABASE {database_name};"""
+			cursor - self.cursor()
+			cursor.execute(query)
+
+		except (Exception, p.DatabaseError) as error:
+			return f"failed to create databse {database_name}, due to {error}"	
+		
